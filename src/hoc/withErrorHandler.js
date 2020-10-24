1a8d6e7c561b245182a127666a2e71ae6732b957
Peter Ndirangu <peter@aetocapital.com>
2020-10-24 17:30:53 +0300

    Redux Advanced Initializing Ingredients in the BurgerBuilder

--- src/hoc/withErrorHandler.js
+++ src/hoc/withErrorHandler.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Modal from '../components/UI/Modal/Modal';
-import Aux from  './_Aux'
+import Aux from  './_Aux';
 const withErrorHandler =(WrappedComponent, axios) =>{
     return class extends Component{
         state = {
